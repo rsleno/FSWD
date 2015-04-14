7ff191e28979698c912b786e635d59d2fda5df7e
rsanz <roberto.sanz@ticketbis.com>
2015-04-14 15:37:14 +0200

    Rename project1 file to media

---
+++ media.py
@@ -0,0 +1,9 @@
+# 
+# Movie Trailer Website
+# The Programming Foundations with Python course will help you get started, specifically Lesson 3a. Be sure to review the course notes provided in the Downloadables section of Lesson 3a, this document will be very helpful understanding how the starter code fits in with your Python Movie class.
+# Make sure that you have Python downloaded on your computer. You may use Python 3, but keep in mind that we are going to be using Python 2. While this shouldn't be too problematic, Python 3 is not backwards compatible and you will be responsible to figure out any discrepancies.
+# Download the file fresh_tomatoes.py which contains the open_movies_page() function that will take in your list of movies and generate an HTML file including this content, producing a website to showcase your favorite movies.
+# Your task is to write a movie class in media.py. To do this, think about what the properties of a movie are that need to be encapsulated in a movie object such as movie titles, box art, poster images, and movie trailer URLs. Look at what open_movies_page() does with a list of movie objects for hints on how to design your movie class.
+# Next you’ll want to write a constructor for the movie class so that you can create instances of movie.
+class Movie():
+	
