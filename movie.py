6f3c5cb42c0550059c1fd01319c22fcb3d8a2c02
rsanz <roberto.sanz@ticketbis.com>
2015-04-15 23:34:27 +0200

    Adding new movies

--- movie.py
+++ movie.py
@@ -1,9 +1,16 @@
 import fresh_tomatoes
 import media
 
-starwars_data = {'title': 'Star Wars. Episode V: The Empire Strikes Back', 'synopsis': 'In the midst of a fierce lightsaber duel with the Sith Lord, Luke faces a startling revelation...', 'poster_image_url': 'http://pics.filmaffinity.com/Star_Wars_Episode_V_The_Empire_Strikes_Back-314829878-large.jpg', 'trailer_youtube_url': 'https://www.youtube.com/watch?v=JNwNXF9Y6kY'}
+clerks_data = {'title': 'Clerks', 'synopsis': 'Dante Hicks is a clerk at a local convenience store in New Jersey', 'poster_image_url': 'http://pics.filmaffinity.com/Clerks-456270250-large.jpg', 'trailer_youtube_url': 'https://www.youtube.com/watch?v=Mlfn5n-E2WE'}
+rd_data = {'title': 'Reservoir Dogs', 'synopsis': 'In the midst of a fierce lightsaber duel with the Sith Lord, Luke faces a startling revelation...', 'poster_image_url': 'http://pics.filmaffinity.com/Reservoir_Dogs-904905830-large.jpg', 'trailer_youtube_url': 'https://www.youtube.com/watch?v=GLPJSmUHZvU'}
+lotr_data = {'title': 'The Lord of the Rings: The Fellowship of the Ring', 'synopsis': 'One ring to rule them all, One ring to find them. One ring to bring them all and in the darkness bind them', 'poster_image_url': 'http://pics.filmaffinity.com/The_Lord_of_the_Rings_The_Fellowship_of_the_Ring-876628736-large.jpg', 'trailer_youtube_url': 'https://www.youtube.com/watch?v=aStYWD25fAQ'}
+snatch_data = {'title': 'Snatch', 'synopsis': 'Turkish, an unlicenced boxing promoter is pulled into trouble when he becomes involved in big time criminal Brick Top, who wants him to arrange a fight and fix it', 'poster_image_url': 'http://pics.filmaffinity.com/Snatch-188547491-large.jpg', 'trailer_youtube_url': 'https://www.youtube.com/watch?v=lUloT3Dh3-E'}
 
-starwars = media.Movie(starwars_data)
-mov =[starwars, starwars, starwars, starwars, starwars, starwars]
+clerks = media.Movie(clerks_data)
+rd = media.Movie(rd_data)
+lotr = media.Movie(lotr_data)
+snatch = media.Movie(snatch_data)
+
+mov =[clerks, lotr, rd, snatch]
 
 fresh_tomatoes.open_movies_page(mov)
